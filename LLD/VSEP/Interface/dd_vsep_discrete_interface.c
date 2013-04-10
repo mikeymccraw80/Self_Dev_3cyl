51359ab6498b7d9fc071a02e67122b6741b830b7
Fan Daowei <daowei.fan@delphi.com>
2013-04-10 14:56:19 +0800

    VSEP, delete "Initialization Dir", move init files to "DeviceDriver Dir"
    Signed-off-by: Fan Daowei <daowei.fan@delphi.com>

---
+++ LLD/VSEP/Interface/dd_vsep_discrete_interface.c
@@ -0,0 +1,31 @@
+//=============================================================================
+//
+//       COPYRIGHT, 2007, Delphi Technologies, Inc. All Rights reserved
+//
+//       Delphi Confidential
+//
+// ============================================================================
+// %full_filespec:            dd_vsep_discrete_interface.c-7:csrc:kok_pt1#1 %
+//
+// created_by:       fz3pdz
+//
+// date_created:     Thu Mar  3 23:47:26 2005
+//
+// %derived_by:      nzdslq %
+//
+// %date_modified:   Fri Feb 16 11:25:43 2007 %
+//
+// %version:         7 %
+//
+// ============================================================================
+// @doc
+//
+// ============================================================================
+// REUSE:
+// DO NOT MODIFY THIS FILE. It contains no configurable parameters.
+// none
+//
+//=============================================================================
+
+#include "dd_vsep.h"
+#include "dd_vsep_discrete_interface.h"
