58bd4478c70f6273ed3035a85444e9a74dfc417a
Fan Daowei <daowei.fan@delphi.com>
2013-04-11 13:10:43 +0800

    vsep, discrete,  seperate variables define from dd_vsep_init_config.
    Signed-off-by: Fan Daowei <daowei.fan@delphi.com>

--- LLD/VSEP/DeviceDriver/dd_vsep_discrete.c
+++ LLD/VSEP/DeviceDriver/dd_vsep_discrete.c
@@ -29,14 +29,15 @@
 
 #include "dd_vsep.h"
 #include "dd_vsep_discrete.h"
+#include "dd_vsep_init_config.h"
 #include "dd_vsep_txd.h"
 #include "dd_vsep_rxd.h"
 #include "dd_vsep_soh.h"
-//#include "dd_port.h"
-
-//#include "dd_config.h"
-//#include "dd_pwm.h"
 #include "dd_vsep_fault.h"
+
+/* PCH spi tx and rx message buffer definiton */
+uint16_t VSEP_PCH_Txd[NUMBER_OF_VSEP][VSEP_PCH_TXD_MESSAGE_MAX];
+uint16_t VSEP_PCH_Rxd[NUMBER_OF_VSEP][VSEP_PCH_RXD_MESSAGE_MAX];
 
 static const uint8_t VSEP_DISCRETE_PCH_CHANNEL_MAP[VSEP_CHANNEL_MAX] =
 {
