bcf366da1450dddd00f473a4fa81495e76895565
Fan Daowei <daowei.fan@delphi.com>
2013-04-19 13:39:50 +0800

    license, update licborrow script file
    Signed-off-by: Fan Daowei <daowei.fan@delphi.com>

--- Build/licborrow.bat
+++ Build/licborrow.bat
@@ -1,9 +1,45 @@
 @echo off
 
-if %1.==. goto help
+echo " display the current license status "
+lmutil lmborrow -status
+echo +====================================
+pause
+
+echo " try to get a new temparory license "
+
+set cur_month=%date:~0,2%
+if %cur_month%==01 set month=Jan
+if %cur_month%==02 set month=Feb
+if %cur_month%==03 set month=Mar
+if %cur_month%==04 set month=Apr
+if %cur_month%==05 set month=May
+if %cur_month%==06 set month=Jun
+if %cur_month%==07 set month=Jul
+if %cur_month%==08 set month=Aug
+if %cur_month%==09 set month=Sep
+if %cur_month%==10 set month=Oct
+if %cur_month%==11 set month=Nov
+if %cur_month%==12 set month=Dec
+REM echo %month%
+set day=%date:~3,2%
+REM echo %day%
+set year=%date:~6,4%
+REM echo %year%
+REM set lictime=%time:~0,5%
+set lictime=23:59
+echo %lictime%
+set line=-
+REM echo %line%
+
+set licdate=%day%%line%%month%%line%%year%
+echo %licdate%
+
+lmutil lmborrow wrsd %licdate% %lictime%
+
+REM if %1.==. goto help
 
 REM if exist foo.c del foo.c
-lmutil lmborrow wrsd %1 %2
+REM lmutil lmborrow wrsd %1 %2
 REM echo main() > foo.c
 REM echo { >> foo.c
 REM echo int a; >> foo.c
@@ -12,9 +48,6 @@ REM C:\BuildArea\Chery_MT22p3_LLD\tools\diab\WIN32\bin\dcc.exe -tPPCE200Z3NEN:si
 REM del foo.c
 REM if exist c.out del c.out
 lmutil lmborrow -status 
-goto end
-
-:help
-echo "<command format>: licborrow 20-Apr-2013 17:00"
-:end
+REM echo "<command format>: licborrow 20-Apr-2013 17:00"
+pause
 @echo on
