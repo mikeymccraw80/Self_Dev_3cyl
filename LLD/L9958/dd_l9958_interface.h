81bbd5a124b3b380d30b5afb007a0bc8aed5d065
Fan Daowei <daowei.fan@delphi.com>
2013-05-03 09:11:00 +0800

    L9958, delete dd_l9958_fault_interface.c for there's nothing in it. rename interface file.
    Signed-off-by: Fan Daowei <daowei.fan@delphi.com>

--- LLD/L9958/dd_l9958_interface.h
+++ LLD/L9958/dd_l9958_interface.h
@@ -5,30 +5,14 @@
 //       COPYRIGHT, 2007, Delphi Technologies, Inc. All Rights reserved
 //
 //       Delphi Confidential
-//
-// ============================================================================
-// %name:            dd_l9958_fault_interface.h %
-//
-// created_by:       hz23l2
-//
-// date_created:     Wed Apr 04 11:15:09 2007
-//
-// %derived_by:       wzmkk7 %
-//
-// %date_modified:    %
-//
-// %version:         mt20u2#2 %
-//
 //=============================================================================
-
+#include "io_type.h"
 #include "fault.h"
 #include "dd_l9958_fault.h"
-#include "hal.h"
 
 //=============================================================================
 // L9958 Interface structures
 //=============================================================================
-extern const Fault_Interface_T L9958_FAULT_INTERFACE;
 
 // L9958 interface to extract common fault flag
 #define L9958_FAULT_Get_OUTPUT_SHORT_CKT_FAULT(x) \
@@ -52,30 +36,32 @@ extern const Fault_Interface_T L9958_FAULT_INTERFACE;
 
 
 // L9958 diagnostic interface
-#define L9958_SPIDiag(action,in_configuration,value)\
-        L9958_SPIFault ## action ## (in_configuration, value)
-	
+// EMS_DIAGNOSTIC_T : value
+#define L9958_SPIDiag(action,value)\
+	L9958_SPIFault ## action ## (value)
+
 // Extract L9958 fault flag
-#define L9958_SPIFaultGet(in_configuration, value) \
+// EMS_DIAGNOSTIC_T : value
+#define L9958_SPIFaultGet(value) \
 	(OUTPUT_SHORT_CKT_FAULT == value? \
-	L9958_FAULT_Get_OUTPUT_SHORT_CKT_FAULT(L9958_Fault_Log[0]):\
+	L9958_FAULT_Get_OUTPUT_SHORT_CKT_FAULT(L9958_Fault_Log):\
 	(OUTPUT_SHORT_CKT_TESTED == value? \
-	L9958_FAULT_Get_OUTPUT_SHORT_CKT_TESTED(L9958_Fault_Log[0]):\
+	L9958_FAULT_Get_OUTPUT_SHORT_CKT_TESTED(L9958_Fault_Log):\
 	(OUTPUT_OPEN_CKT_FAULT == value? \
-	L9958_FAULT_Get_OUTPUT_OPEN_CKT_FAULT(L9958_Fault_Log[0]):\
+	L9958_FAULT_Get_OUTPUT_OPEN_CKT_FAULT(L9958_Fault_Log):\
 	(OUTPUT_OPEN_CKT_TESTED == value? \
-	L9958_FAULT_Get_OUTPUT_OPEN_CKT_TESTED(L9958_Fault_Log[0]):0))))
+	L9958_FAULT_Get_OUTPUT_OPEN_CKT_TESTED(L9958_Fault_Log):0))))
 
 // Reset L9958 fault flag
-#define L9958_SPIFaultClear(in_configuration, value) \
+// EMS_DIAGNOSTIC_T : value
+#define L9958_SPIFaultClear(value) \
 	(OUTPUT_SHORT_CKT_FAULT == value? \
-	L9958_Fault_Log[0]=L9958_FAULT_Clear_OUTPUT_SHORT_CKT_FAULT(L9958_Fault_Log[0]):\
+	L9958_Fault_Log=L9958_FAULT_Clear_OUTPUT_SHORT_CKT_FAULT(L9958_Fault_Log):\
 	(OUTPUT_SHORT_CKT_TESTED == value? \
-	L9958_Fault_Log[0]=L9958_FAULT_Clear_OUTPUT_SHORT_CKT_TESTED(L9958_Fault_Log[0]):\
+	L9958_Fault_Log=L9958_FAULT_Clear_OUTPUT_SHORT_CKT_TESTED(L9958_Fault_Log):\
 	(OUTPUT_OPEN_CKT_FAULT == value? \
-	L9958_Fault_Log[0]=L9958_FAULT_Clear_OUTPUT_OPEN_CKT_FAULT(L9958_Fault_Log[0]):\
+	L9958_Fault_Log=L9958_FAULT_Clear_OUTPUT_OPEN_CKT_FAULT(L9958_Fault_Log):\
 	(OUTPUT_OPEN_CKT_TESTED == value? \
-	L9958_Fault_Log[0]=L9958_FAULT_Clear_OUTPUT_OPEN_CKT_TESTED(L9958_Fault_Log[0]):0))))
-
+	L9958_Fault_Log=L9958_FAULT_Clear_OUTPUT_OPEN_CKT_TESTED(L9958_Fault_Log):0))))
 
 #endif // DD_L9958_FAULT_INTERFACE_H
