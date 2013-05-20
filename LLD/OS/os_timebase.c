2455cc0e7ab337ea0cc273b91c0ea0e8edfc56ea
daoweifan <daowei.fan@delphi.com>
2013-05-20 11:04:37 +0800

    OS Timer base, improve...
    Signed-off-by: daoweifan <daowei.fan@delphi.com>

--- LLD/OS/os_timebase.c
+++ LLD/OS/os_timebase.c
@@ -26,7 +26,7 @@ void OS_RTI_1ms_INT(void)
 {
 	PIT_INTERRUPT_Clear_Pending(PIT_CHANNEL_RTI);
 
-	OS_RTI_1ms_Count = (OS_RTI_1ms_Count > 10)? 1 : OS_RTI_1ms_Count++;
+	OS_RTI_1ms_Count = (OS_RTI_1ms_Count >= 10)? 1 : OS_RTI_1ms_Count++;
 
 	/* 1ms task reference */
 	RTI_Flags.bf.TimeFor1ms = 1;
