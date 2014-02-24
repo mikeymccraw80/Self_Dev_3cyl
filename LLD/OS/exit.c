ae837661b522cb6e6819a433adca17dd1069b8b9
daoweifan <daowei.fan@delphi.com>
2014-02-24 16:55:38 +0800

    exit, update the coding style.
    Signed-off-by: daoweifan <daowei.fan@delphi.com>

--- LLD/OS/exit.c
+++ LLD/OS/exit.c
@@ -12,33 +12,24 @@
 //=============================================================================
 // exit
 //=============================================================================
- void exit(void)
+void exit(void)
 {
-   // Set the watchdog timeout to 300ms for flash erase/program
-   SWT_Set_Timeout_Value(SWT_TIMEOUT_VALUE_INIT) ;
-   SWT_Service_WatchDog();
+    // Set the watchdog timeout to 300ms for flash erase/program
+    SWT_Set_Timeout_Value(SWT_TIMEOUT_VALUE_INIT) ;
+    SWT_Service_WatchDog();
 
+    EEPROM_Backup_MFG_NVM_Block( true );
+    EEPROM_Backup_Vehicle_NVRAM_Block();
+    
+    // only LCI will do the instrumentation operation, backup cal 
+    if(CPU_LCI == CPU_Info) {
+       INST_Backup_Working_Page();
+    }
 
-   EEPROM_Backup_MFG_NVM_Block( true );
-   EEPROM_Backup_Vehicle_NVRAM_Block();
-
-   // only LCI will do the instrumentation operation
-   if(CPU_LCI == CPU_Info)
-   {
-      // backup cal 
-      INST_Backup_Working_Page();
-   }
-
-
-   //Set TOD low now since all power down processing is complete
-    //  Deactivate_TimeOffDelay();
+    //Set TOD low now since all power down processing is complete
     HAL_GPIO_SET_TODO_Enable(false);
 
     INTC_EXCEPTION_Halt();
-   // Wait in an infinite loop for the power supply to shutdown
-   do {} while (true);
-
+    // Wait in an infinite loop for the power supply to shutdown
+    do {} while (true);
 }
-
-
-
