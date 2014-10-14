230933e2b663821454475e6b9e20444e48e26c47
daoweifan <daowei.fan@delphi.com>
2014-10-14 11:18:30 +0800

    limphome, set "STALL_DETECTED" flag to false, when detect "no crank signal"           this change can fix the "rpm" bug when engine stalled.
    Signed-off-by: daoweifan <daowei.fan@delphi.com>

--- LLD/Complex_IO/CAM/dd_cam.c
+++ LLD/Complex_IO/CAM/dd_cam.c
@@ -625,6 +625,7 @@ void CAM_Edge_Process( uint32_t in_cam_sensor )
 			if (CRANK_Get_Diag_Tooth_Cnt() == 0) {
 				CAM1_CRANK_Diagnose_Fail_Count ++;
 				OS_Engine_Start_Crank();
+				CRANK_Set_Flag( CRANK_FLAG_STALL_DETECTED, false );
 			} else {
 				CAM1_CRANK_Diagnose_Fail_Count = 0;
 			}
