5e2457ac33d609acbf679e92902b33cf1c48887b
daoweifan <daowei.fan@delphi.com>
2014-11-05 15:31:52 +0800

    canobd, add can obd transfer layer config file.
    Signed-off-by: daoweifan <daowei.fan@delphi.com>

---
+++ LLD/Communications/OBD/OBDLIB/io_dcan_config.c
@@ -0,0 +1,179 @@
+//=============================================================================
+//       COPYRIGHT 2005, Delphi Technologies, Inc. All Rights reserved
+//                             Delphi Confidential
+//
+// --------------------------------------------------------------------------
+// %name:            io_dcan_config.c %
+//
+// created_by:       qznx5b
+//
+// date_created:     Wed. Mar. 2 2005
+//
+// %derived_by:      tzwfxv %
+//
+// %date_modified:       %
+//
+// %version:         4 %
+//
+//=============================================================================
+/******************************************************************************
+* APP Include Files
+******************************************************************************/
+#include "obdspcfg.h"
+#include "obdsfapp.h" /* Get_Receive_Message_Number_From_Message_ID */
+/******************************************************************************
+* CAN OBD Service Include Files
+******************************************************************************/     
+#include "io_dcan_config.h" 
+#include "cn_io_diagnostic.h" /* For Message_Length_Mismatch                 */
+/******************************************************************************
+* CAN OBD NW Layer Include Files
+******************************************************************************/
+
+/******************************************************************************
+ *  Global Variables
+ *****************************************************************************/
+uint8_t           canobd_tx_rx_buffer[MESSAGE_NUM_OF_CANOBD][8];
+VioCanRxBufferStructType VioCanRxBuffer[RX_MSG_BUFFER_NUMBER];
+/******************************************************************************
+ *  Global Variable Declarations
+ *****************************************************************************/
+
+extern const uint8_t BufferSizeRxMesgControlTable;
+
+/**********************************************************************/
+/*** Function prototype                                             ***/
+/**********************************************************************/
+void Callback_Application_CANOBD( uint32_t message_id); 
+void Notify_Application_CANOBD( uint32_t message_id);
+
+/*********************************************************************/
+/*** Define CANOBD message parameter  table         ***/
+/*********************************************************************/
+CAN_Message_Parameter_T   CANOBD_Message_Parameter_Table[MESSAGE_NUM_OF_CANOBD] =
+{
+   {CanId5e8,   8, 1, &canobd_tx_rx_buffer[0][0], Callback_Application_CANOBD,HAL_CAN_DEVICE_A,HAL_CAN_MESSAGE_DIRECTION_TRANSMIT }
+  ,{CanId7e8,   8, 1, &canobd_tx_rx_buffer[1][0], Callback_Application_CANOBD,HAL_CAN_DEVICE_A,HAL_CAN_MESSAGE_DIRECTION_TRANSMIT }
+  ,{CanId7df,   8, 1, &canobd_tx_rx_buffer[2][0], Notify_Application_CANOBD, HAL_CAN_DEVICE_A,HAL_CAN_MESSAGE_DIRECTION_RECEIVE}
+  ,{CanId7e0,   8, 1, &canobd_tx_rx_buffer[3][0], Notify_Application_CANOBD, HAL_CAN_DEVICE_A,HAL_CAN_MESSAGE_DIRECTION_RECEIVE}
+  ,{CanIdCAL,   8, 1, &canobd_tx_rx_buffer[4][0], Notify_Application_CANOBD, HAL_CAN_DEVICE_A,HAL_CAN_MESSAGE_DIRECTION_RECEIVE}
+};
+
+/****************************************************************/
+/***                     RECEPTION TASKS                      ***/
+/****************************************************************/
+CanRxMessageControlType CanRxMesgControlTable [] = {
+/* Functional Request EOBD */
+  CanId7df,
+  BufferCanId7df,
+  CanId7dfRcvdEvent
+  ,
+/* Diagnostic Information Usdt */
+  CanId7e0,
+  BufferCanId7e0,
+  CanId7e0RcvdEvent
+  ,
+/* Diagnostic Information Usdt */
+  CanIdCAL,
+  BufferCanIdCAL,
+  CanIdCALRcvdEvent
+};
+
+#define RxMsgNbWithRxTimingsControlled (sizeof (CanRxMesgControlTable) / sizeof (CanRxMessageControlType))
+const uint8_t BufferSizeRxMesgControlTable = RxMsgNbWithRxTimingsControlled;
+
+void Notify_Application_CANOBD( uint32_t  message_id )
+{
+   VioCanRxBufferStructType * PtrOnVioCanRxBuffer;
+   Can8DataBytesArrayType *Can8DataBytesArrayPtr;
+   uint16_t PreviousRxBufferstate;
+   uint8_t CanBuffer=0;
+   uint8_t found = false;
+   uint8_t CAN_message_number = 0;
+   uint8_t  Transferlayerbuffernumber = 0;
+
+   /* Bypass message length check because CAN-OBD supports multi-frame mode */
+   Clear_Message_Length_Mismatch();
+
+   Transferlayerbuffernumber = Get_Receive_Message_Number_From_Message_ID(message_id); //mzyqz4-sep10
+ 
+   for( CAN_message_number = 0;
+        CAN_message_number <  BufferSizeRxMesgControlTable ;
+        CAN_message_number++ )
+   {
+      if( CanRxMesgControlTable[CAN_message_number].CAN_message_ID == message_id )
+      {
+         found  = 1;
+         CanBuffer = CAN_message_number;
+         break;
+      }
+   }
+
+   if (found != false)
+   {
+      // we have found a matching buffer entry,lets try to process the received msg.
+      
+     //CanBuffer = CanRxMesgControlTable [CAN_message_number].MessageBufferNumber;
+
+      PtrOnVioCanRxBuffer = &(VioCanRxBuffer [CanBuffer]);
+
+      PreviousRxBufferstate = VioCanRxBuffer[CanBuffer].State; 
+      VioCanRxBuffer[CanBuffer].State = VioRxBufferBusy;
+
+      if ( Get_OBD_Message( Transferlayerbuffernumber, PtrOnVioCanRxBuffer->DataBytes ) )
+      {
+         if (PreviousRxBufferstate == VioRxBufferEmpty) 
+         {
+            VioCanRxBuffer[CanBuffer].State = VioRxBufferFull;
+         }
+         else
+         {
+            VioCanRxBuffer[CanBuffer].State = VioRxBufferOverrun;
+         }
+         Can8DataBytesArrayPtr =
+                   (Can8DataBytesArrayType *) &(VioCanRxBuffer [CanBuffer].DataBytes);
+        (*CanRxMesgControlTable [CanBuffer].Task) (Can8DataBytesArrayPtr);
+
+      }
+   } 
+}
+
+void Callback_Application_CANOBD( uint32_t   CanId)
+{
+   uint16_t in_msg_obj;
+   Can8DataBytesArrayType *Can8DataBytesArrayPtr;
+
+   in_msg_obj = Get_Transmit_Message_Number_From_Message_ID(CanId);
+   Can8DataBytesArrayPtr = (Can8DataBytesArrayType *)
+                           CANOBD_Message_Parameter_Table[ in_msg_obj -VyCANH_Current_message_Number ].CAN_buffer_ptr; 
+   switch (CanId)
+   {
+      case CanId5e8:
+        CanId5e8RcvdEvent(Can8DataBytesArrayPtr);
+        break;
+      case CanId7e8:
+        CanId7e8RcvdEvent(Can8DataBytesArrayPtr);
+        break;
+   }
+}
+
+CAN_Message_Parameter_T Get_CANOBD_Message_Parameter_Table(uint8_t index)
+{
+    return CANOBD_Message_Parameter_Table[index];
+}
+
+void Initial_CANOBD_CAL_ID(uint16_t CANID)
+{
+    CANOBD_Message_Parameter_Table[IndxCanIdCAL].CAN_message_ID = CANID;
+    CanRxMesgControlTable[TXIndxCanIdCAL].CAN_message_ID = CANID;
+}
+
+/**********************************************************************
+*
+* Revision History:                                                 
+*                                                                    
+* Rev.   YYMMDD Who RSM# Change                                  
+* ------ ------ --- ---- ---------------------------------------
+*3     121017 xll  add CAN ID 0x600 for extended service functional addressing
+*4     121224 xjj  add function Get_CANOBD_Message_Parameter_Table & Initial_CANOBD_CAL_ID
+**********************************************************************/
---
+++ LLD/Communications/OBD/OBDLIB/io_dcan_config.h
@@ -0,0 +1,96 @@
+#ifndef IO_DCAN_CONFIG_H
+#define IO_DCAN_CONFIG_H
+//=============================================================================
+//       COPYRIGHT 2005, Delphi Technologies, Inc. All Rights reserved
+//                             Delphi Confidential
+//
+// --------------------------------------------------------------------------
+// %name:            io_dcan_config.h %
+//
+// created_by:       qznx5b
+//
+// date_created:     Wed. Mar. 2 2005
+//
+// %derived_by:      zzrfyj %
+//
+// %date_modified:         %
+//
+// %version:         6 %
+//
+//=============================================================================
+
+//=============================================================================
+//       Include Files
+//=============================================================================
+#include "io_type.h"
+#include "dcantran.h"
+#include "obdspcfg.h"
+#include "cn_io_transfer.h"/*CAN_Message_Parameter_T*/
+#include "hal_can.h"
+
+//=============================================================================
+//       Macro Definitions
+//=============================================================================
+
+/* Polled / Interrupt Mode selection values for Transmit, Receive 
+ *  and Error-Handler routines
+ */
+typedef struct
+{
+   uint16_t       CAN_message_ID; //mzyqz4-added newly.
+   uint8_t        MessageBufferNumber;
+   void             (*Task) (Can8DataBytesArrayType *);  /*--- address of the can task to
+                                                    execute on the reception of a message
+                                                    identified by its CAN ID  ---*/
+}CanRxMessageControlType;
+
+#define Buffer0        (0x0)
+#define Buffer1        (0x1)
+#define Buffer2        (0x2)
+/*--------------------------------------------------------------------*/
+/*--- AFFECTATION OF CAN MESSAGES TO THE DIFFERENT RECEIVE BUFFERS ---*/
+/*--------------------------------------------------------------------*/
+/*---      .                                                       ---*/
+/*---     / \     the following definition must follow the naming  ---*/
+/*---    / | \    convention:                                      ---*/
+/*---   /  |  \   "Buffer"+"Can_message_ID"                        ---*/
+/*---  /   |   \                                                   ---*/
+/*---  ---------                                                   ---*/
+/*--------------------------------------------------------------------*/
+
+#define BufferCanId7df                (Buffer0)
+#define BufferCanId7e0                (Buffer1)
+#define BufferCanIdCAL                (Buffer2)
+
+/*** WARNING : Buffer can not be allocated beyond 24 as 8 buffers are used for Transmit msgs ***/
+/*-----------------------------------------------------------------*/
+/*--- VIO RECEIVE MESSAGES TYPES DEFINITION                        */
+/*--- (for Received messages which have a buffer in Virtual IO) ---*/
+/*-----------------------------------------------------------------*/
+#define VioRxBufferNotActive (0)
+#define VioRxBufferEmpty     (1)
+#define VioRxBufferBusy      (2)
+#define VioRxBufferFull      (3)
+#define VioRxBufferOverrun   (4)
+
+/*****************************************/
+/*            CAN OBD config             */
+/*****************************************/
+/*****************************************/
+#if (XeSYST_CANOBD_SrvsEnable == CeSYST_AVAILABLE)
+#define TX_MESSAGE_NUM_OF_CANOBD (2)
+#define RX_MESSAGE_NUM_OF_CANOBD (3)
+#else
+#define TX_MESSAGE_NUM_OF_CANOBD (0)
+#define RX_MESSAGE_NUM_OF_CANOBD (0)
+#endif
+
+#define MESSAGE_NUM_OF_CANOBD    (TX_MESSAGE_NUM_OF_CANOBD + RX_MESSAGE_NUM_OF_CANOBD)
+
+extern CAN_Message_Parameter_T Get_CANOBD_Message_Parameter_Table(uint8_t index);
+extern void Initial_CANOBD_CAL_ID(uint16_t CANID);
+
+
+
+#endif
+
