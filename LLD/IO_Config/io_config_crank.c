10898cac8922868fb74d10d0d70badd0bc3b5426
Zhang Hongfei <hongfei.zhang@delphi.com>
2016-10-18 10:29:35 +0800

    update the GAP detect threshold.

--- LLD/IO_Config/io_config_crank.c
+++ LLD/IO_Config/io_config_crank.c
@@ -13,7 +13,7 @@ const MCD5408_Channel_Init_T EPPWMT_INIT =
    true,                                       //gap_detect_enable;
    CRANK_TEETH_PER_GAP,                                          //gap_size;
    EPPE_1_X__Y_1,                              //gap_detect_method - EPPE_1_X__Y_1 or EPPE_PERCENT_PERIOD
-   0x73,                                       //gap_detect_ratio; - in percentage - 0 to FF (0 to 100%)
+   0x80,                                       //gap_detect_ratio; - in percentage - 0 to FF (0 to 100%)
    EPPE_HW_MIN_PERIOD,                         //filter_type; - EPPE_HW_MIN_PERIOD or EPPE_SW_MIN_PERIOD
    (uint16_t)(TPU_A_TCR1_CLOCK_FREQ*0.00002),  //filter_value in TCR1 count;
    EPPWMT_ANGLE_MODE_PLL,                      //angle_mode;
